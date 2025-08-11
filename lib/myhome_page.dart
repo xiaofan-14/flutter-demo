01884b74e20db76467011357faefd5892109195c
xiaofan <ningfengzeng@gmail.com>
2025-08-11 20:28:56 +0800

    首页菜单

--- lib/myhome_page.dart
+++ lib/myhome_page.dart
@@ -7,6 +7,15 @@ class MyhomePage extends StatefulWidget {
   State<MyhomePage> createState() => _MyHomePageState();
 }
 
+int selectIndex = 0;
+List<String> categoryList = [
+  'Bast nature',
+  'Most viewed',
+  "Recommend",
+  "Newly discover",
+  "Peace",
+];
+
 class _MyHomePageState extends State<MyhomePage> {
   @override
   Widget build(BuildContext context) {
@@ -27,11 +36,91 @@ class _MyHomePageState extends State<MyhomePage> {
                   height: MediaQuery.of(context).size.height / 1.4,
                   width: MediaQuery.of(context).size.width,
                   decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Color(0xfffffbff), Color(0xfff3ecee)],
+                    ),
                     borderRadius: BorderRadius.only(
                       bottomLeft: Radius.circular(70),
                     ),
                   ),
-                  color: Colors.amber,
+                  child: Stack(
+                    children: [
+                      SizedBox(
+                        height: 261,
+                        width: double.infinity,
+                        child: SafeArea(
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 20),
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 20),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Image.asset(
+                                        'Images/more.png',
+                                        height: 30,
+                                      ),
+                                      Image.asset(
+                                        "Images/search.png",
+                                        height: 30,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                const SizedBox(height: 40),
+                                Row(
+                                  children: const [
+                                    Text(
+                                      "Discover",
+                                      style: TextStyle(
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 15),
+                                natrueSelector(),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 245,
+                        child: SizedBox(
+                          height: 400,
+                          width: MediaQuery.of(context).size.width,
+                          child: ListView.builder(
+                            itemCount: categoryList.length,
+                            physics: const BouncingScrollPhysics(),
+                            shrinkWrap: true,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.only(right: 20),
+                                child: Text(
+                                  categoryList[index],
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                    color: selectIndex == index
+                                        ? Color(0xffa36c88)
+                                        : Color(0xffe2cbd4),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
@@ -40,8 +129,12 @@ class _MyHomePageState extends State<MyhomePage> {
               child: Container(
                 height: MediaQuery.of(context).size.height / 3.49,
                 width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Colors.amber
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomCenter,
+                    colors: [Color(0xfffffbff), Color(0xfff3ecee)],
+                  ),
                 ),
               ),
             ),
@@ -50,8 +143,8 @@ class _MyHomePageState extends State<MyhomePage> {
               child: Container(
                 height: MediaQuery.of(context).size.height / 3.49,
                 width: MediaQuery.of(context).size.width,
-                color: Colors.white,
                 decoration: const BoxDecoration(
+                  color: Colors.white,
                   borderRadius: BorderRadius.only(
                     topRight: Radius.circular(70),
                   ),
@@ -61,6 +154,49 @@ class _MyHomePageState extends State<MyhomePage> {
           ],
         ),
       ),
+    );
+  }
+
+  Stack natrueSelector() {
+    return Stack(
+      children: [
+        Positioned(
+          bottom: -10,
+          left: 45,
+          child: Text(
+            '.',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 40,
+              color: const Color(0xffa36c88),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 40,
+          child: ListView.builder(
+            itemCount: categoryList.length,
+            physics: const BouncingScrollPhysics(),
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(right: 20),
+                child: Text(
+                  categoryList[index],
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: selectIndex == index
+                        ? Color(0xffa36c88)
+                        : Color(0xffe2cbd4),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
