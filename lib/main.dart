faed454a801e17c636dbb0687166ba212d41cad7
xiaofan <ningfengzeng@gmail.com>
2025-09-21 14:45:54 +0800

    移除之前的样式，重新学习

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter_application_1/myhome_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,15 +7,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          textTheme: GoogleFonts.playfairDisplayTextTheme()
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: Center(
+          child: Text('Hello, World!'),
         ),
-        home: const MyhomePage(),
-      );
+      ),
+    );
   }
 }
